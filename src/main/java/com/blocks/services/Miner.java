8cca48adf47e39a350d84b204a8b0967d3a627e1
Ryan Fritts <anvilsnare@gmail.com>
2017-07-29 19:35:27 -0500

    Refactored Miner

--- src/main/java/com/blocks/services/Miner.java
+++ src/main/java/com/blocks/services/Miner.java
@@ -3,39 +3,62 @@ package com.blocks.services;
 import com.blocks.models.Block;
 import com.blocks.utils.BlockUtil;
 
-import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class Miner {
 
     private static int leadingZeros = 5;
     private static long nonce = 0;
-    private static long lastSecond;
+    private static long previousSecond;
     private static long lastMeasuredNonce = 0;
     private static long hashRate;
 
-    public static boolean mineBlock(Block block) throws UnsupportedEncodingException, NoSuchAlgorithmException {
-        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
-        messageDigest.update(block.getPayload().getBytes("UTF-8"));
-        byte[] digest = messageDigest.digest();
-        String initialHash = String.format("%064x", new java.math.BigInteger(1, digest));
-        String noncePlusHash;
+    public static void mineBlock(Block block) {
+        // MessageDigest is the java object that performs the actual hashing
+        MessageDigest messageDigest = getNewMessageDigest();
+        String initialHash = getInitialHash(block, messageDigest);
         String blockHash;
-        lastSecond = System.nanoTime();
         do {
-            noncePlusHash = String.valueOf(nonce) + initialHash + block.getPreviousPayloadHash();
-            messageDigest.update(noncePlusHash.getBytes("UTF-8"));
-            blockHash = String.format("%064x", new java.math.BigInteger(1, messageDigest.digest()));
-            printHashingInfo();
+            blockHash = getBlockHash(block, messageDigest, initialHash);
+            printHashInfo();
         } while (!isValidNonceHash(blockHash));
-        block.setMinedPayloadHash(blockHash);
-        block.setNonce(nonce);
-        block.setMined(true);
-        nonce = 0;
-        lastMeasuredNonce = 0;
-        BlockUtil.printBlock(block);
-        return true;
+        postMinedInfoToBlock(block, blockHash);
+        resetMiner();
+        BlockUtil.printMinedBlock(block);
+    }
+
+    private static String getInitialHash(Block block, MessageDigest messageDigest) {
+        updateMessageDigest(messageDigest, block.getPayload());
+        byte[] digest = messageDigest.digest();
+        return String.format("%064x", new java.math.BigInteger(1, digest));
+    }
+
+    private static String getBlockHash(Block block, MessageDigest messageDigest, String initialHash) {
+        updateMessageDigest(messageDigest, String.valueOf(nonce) + initialHash + block.getPreviousPayloadHash());
+        return String.format("%064x", new java.math.BigInteger(1, messageDigest.digest()));
+
+    }
+
+    private static void updateMessageDigest(MessageDigest messageDigest, String string) {
+        try {
+            messageDigest.update(string.getBytes("UTF-8"));
+        } catch (Exception exp) {
+            System.out.println("Something went terribly wrong when trying to update messageDigest. "
+                    + exp.getMessage());
+            System.exit(1);
+        }
+    }
+
+    private static MessageDigest getNewMessageDigest() {
+        MessageDigest messageDigest = null;
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+        } catch (Exception exp) {
+            System.out.println("Something went terribly wrong when trying to create a new messageDigest. "
+                    + exp.getMessage());
+            System.exit(1);
+        }
+        return messageDigest;
     }
 
     private static boolean isValidNonceHash(String hash) {
@@ -48,13 +71,27 @@ public class Miner {
         return true;
     }
 
-    private static void printHashingInfo() {
-        if (System.nanoTime() - lastSecond > 1000000000) {
-            lastSecond = System.nanoTime();
+    private static void printHashInfo() {
+        if (previousSecond == 0) {
+            previousSecond = System.nanoTime();
+        }
+        if (System.nanoTime() - previousSecond > 1000000000) {
+            previousSecond = System.nanoTime();
             hashRate = nonce - lastMeasuredNonce;
             lastMeasuredNonce = nonce;
         }
         System.out.print("Current Nonce: " + nonce + " | Current hashRate: " + hashRate + " hps" + "\r");
+    }
+
+    private static void postMinedInfoToBlock(Block block, String blockHash) {
+        block.setMinedPayloadHash(blockHash);
+        block.setNonce(nonce);
+        block.setMined(true);
+    }
+
+    private static void resetMiner() {
+        nonce = 0;
+        lastMeasuredNonce = 0;
     }
 
 }
--- src/main/java/com/blocks/utils/BlockUtil.java
+++ src/main/java/com/blocks/utils/BlockUtil.java
@@ -9,7 +9,8 @@ public class BlockUtil {
         block.setPayload(new Gson().toJson(block.getListOfVerifiedTransactions()));
     }
 
-    public static void printBlock(Block block) {
+    public static void printMinedBlock(Block block) {
+        System.out.println("-----------------------------                                              ");
         System.out.println("Block Payload: " + block.getPayload());
         System.out.println("Mined hash: " + block.getMinedPayloadHash());
         System.out.println("Final nonce: " + block.getNonce());
